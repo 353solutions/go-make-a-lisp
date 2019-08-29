d2ff2c9b8fad29ef901d5602cad417516e7e9ba6
Miki Tebeka <miki.tebeka@gmail.com>
2019-08-29 10:41:18 +0300

    add space right

--- humble-full/humble.go
+++ humble-full/humble.go
@@ -72,7 +72,7 @@ type Token string
 // Tokenize splits the t list of tokens
 func Tokenize(code string) []Token {
 	code = strings.Replace(code, "(", " ( ", -1)
-	code = strings.Replace(code, ")", " )", -1)
+	code = strings.Replace(code, ")", " ) ", -1)
 	var tokens []Token
 	for _, tok := range strings.Fields(code) {
 		tokens = append(tokens, Token(tok))
